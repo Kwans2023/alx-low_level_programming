8a8618938b07d8443e68315f187047bb2e422d3f
Kwans2023 <kwansemaq@gmail.com>
2023-04-27 10:16:23 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *Description - 'ptints alphabets in lower and uppercase'
+  *Return: always 0 (Success)
+  */
+
+int main(void)
+{
+	int a = 97;
+	int b = 65;
+
+	while (if a <= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 90)
+	{
+		putchar(b);
+		b++;
+	}
+	putchar("\n");
+	return (0);
+}
