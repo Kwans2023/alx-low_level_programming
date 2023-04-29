898cbc753bf2df390e7a73080004e84a925ee14e
Kwans2023 <kwansemaq@gmail.com>
2023-04-28 19:03:17 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_numbers - Entry point
+* Description - A function that print numbers from 0 to 9
+* Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+
+{
+
+int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		_putchar(x);
+	}
+		_putchar('\n');
+}
