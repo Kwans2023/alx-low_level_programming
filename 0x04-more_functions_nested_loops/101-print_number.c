f8328c9d65360458216a06656088c890c2d28fd2
Kwans2023 <kwansemaq@gmail.com>
2023-04-28 19:46:32 -0700

    task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ * Description: printing integers
+ * @n: Input integer parameter
+ */
+void print_number(int n)
+{
+	unsigned int b = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		b = -b;
+	}
+	if (b / 10)
+	{
+		print_number(b / 10);
+	}
+	_putchar(b % 10 + 48);
+}
