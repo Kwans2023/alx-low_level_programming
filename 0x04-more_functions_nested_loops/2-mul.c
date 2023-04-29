e2d34acca85d5fca98c75789d0f730c767e123c3
Kwans2023 <kwansemaq@gmail.com>
2023-04-28 18:58:04 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* mul - Entry point
+* Description: Multiplication of two integers
+* @x: first integer inputed
+* @y: second integer inputed
+* Return:  result
+*/
+
+int mul(int x, int y)
+
+{
+	return (x * y);
+}
