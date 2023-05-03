f92df70ee644e594c4884ed41bc1b97e07cd05e4
Kwans2023 <kwansemaq@gmail.com>
2023-05-03 09:20:33 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - Create a pointer to an int variable with name n, that points
+  * to an int value 98
+  * @n: Pointer to an interger variable
+  * Return: always 0
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
